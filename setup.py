2d87e1abb4ec324a6e5323474bcd910f827d3dbe
Megan <megan.lkm@gmail.com>
2016-06-10 11:54:26 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from pip.req import parse_requirements
 from pip.download import PipSession
-from setuptools import setup, find_packages
+from setuptools import setup
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 VERSION = open(os.path.join(BASEDIR, 'VERSION')).read().strip()
@@ -9,13 +9,21 @@ INSTALL_REQS = parse_requirements('requirements.pip', session=PipSession())
 
 setup(
     name='spylogger',
+    packages=['spylogger'],
     version=VERSION,
-    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     description='Python logging for SPS libraries.',
     long_description=open('README.md').read(),
     url='https://github.com/SPSCommerce/spylogger',
     author='meganlkm',
     author_email='mwood@spscommerce.com',
-    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+    install_requires=[str(ir.req) for ir in INSTALL_REQS],
+    keywords=['logging'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Logging',
+    ]
 )
