b6405094891a2436d15e61d0ee92ceadf2bce614
Megan <megan.lkm@gmail.com>
2016-06-10 12:50:49 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from pip.req import parse_requirements
 from pip.download import PipSession
 from setuptools import setup
 
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
-VERSION = open(os.path.join(BASEDIR, 'VERSION')).read().strip()
 INSTALL_REQS = parse_requirements('requirements.pip', session=PipSession())
 
 setup(
     name='spylogger',
     packages=['spylogger'],
-    version=VERSION,
+    version=open('VERSION').read().strip(),
     include_package_data=True,
     description='Python logging library',
     long_description=open('README.rst').read(),
     url='https://github.com/SPSCommerce/spylogger',
     author='meganlkm',
-    author_email='mwood@spscommerce.com',
+    author_email='webapps@spscommerce.com',
     install_requires=[str(ir.req) for ir in INSTALL_REQS],
     keywords=['logging'],
     classifiers=[
