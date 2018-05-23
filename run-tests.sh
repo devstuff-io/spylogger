b0ad264f256b62de155a7ed820ad9080c302ca09
Megan <megan.lkm@gmail.com>
2018-05-23 12:34:22 -0500

    setup tests

---
+++ run-tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PACKAGE_NAME="spylogger"
+
+with_rednose="--rednose --force-color"
+with_coverage="--cover-html-dir=${BASE_DIR}/htmlcov --with-coverage --cover-html --cover-package=${PACKAGE_NAME} --cover-erase --cover-branches"
+with_doctest="--with-doctest"
+
+test -z $1 || dotests="--tests=${1}"
+
+exec nosetests ${with_rednose} -s -v ${with_doctest} ${with_coverage} --where ${BASE_DIR}/tests ${dotests}
---
+++ tests/test_spylogger.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+import spylogger
+
+
+class SpyloggerTest(TestCase):
+
+    def test_nothing(self):
+        self.assertTrue(True)
